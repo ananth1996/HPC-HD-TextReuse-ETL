ccf63b21e248e36a97ec873b6bdf08236f38c5c8
Ananth Mahadevan <ananth.mahadevan@helsinki.fi>
2023-04-19 13:19:28 +0000

    Refactors code for end-to-end etl
    ETL for reading zip file and writing parquet

--- zip2parquet.py
+++ zip2parquet.py
@@ -11,27 +11,19 @@ import smart_open
 import logging
 import argparse
 import random
-project_root = Path(__file__).parent.resolve()
-#%%
+from pyspark.sql import SparkSession
+import os
+import ijson 
+from functools import partial
+project_root = Path(__file__).resolve()
+#%% 
 
-# import findspark
-# import os
-# findspark.init()
-# from pyspark.sql import SparkSession
-# os.environ["JAVA_HOME"] = "/appl/soft/bio/java/openjdk/jdk8u312-b07/"
-# spark = (SparkSession
-# 		.builder
-#         .master(f"local[1]")
-# 		.appName("ETL")
-# 		.config("spark.driver.memory","1g")
-# 		# .config("spark.local.dir",os.environ["LOCAL_SCRATCH"])
-# 		.getOrCreate())
-# spark.sparkContext.setLogLevel("WARN")
-# sc = spark.sparkContext
 def get_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("fname",help="Name of the file to process",type=str)
     parser.add_argument("--local",help="Whether to use local file",action="store_true")
+    parser.add_argument("--s3_bucket",type=str,help="The s3 bucket where zip file is stored",default="textreuse-raw-data")
+    parser.add_argument("--num_partitions",type=int,default=200,help="Number of partitions for spark")
     parser.add_argument(
     '-d', '--debug',
     help="Print lots of debugging statements",
@@ -43,68 +35,97 @@ def get_parser():
         help="Be verbose",
         action="store_const", dest="loglevel", const=logging.INFO,
     )
-    parser.add_argument("--shuffle",action="store_true")
-    parser.add_argument("--seed",type=int)
-
     return parser
+#%%
+def get_jsons(fileinfo,fname:str,cred:dict):
+    if "_fileobj" not in globals():
+        logger.info("Creating global variables")
+        global _fileobj
+        global _session
+        global _s3
+        global _transport_params
+        global _zf
+        # _fileobj = 1
+        _session = boto3.session.Session()
+        _s3 = session.client(
+            service_name='s3',
+            **cred["default"]
+        )
+        # _fileobj =
+        _fileobj = smart_open.open(fname,"rb",compression='disable',transport_params=dict(client=_s3))
+        _zf = zipfile.ZipFile(_fileobj)
+    with _zf.open(fileinfo) as fp:
+        try:
+            # open tar.gz file
+            logger.info(f"Opening tar file {fp}")
+            with tarfile.open(fileobj=fp,debug=3,mode="r|*") as tf:
+                # go through members in tarfile
+                logger.debug("Looping over members")
+                try:
+                    for member in tf:
+                        logger.debug(f"{member=}")
+                        logger.debug(f"{tf.fileobj=}, {member.offset_data=},{member.size=}, {member.sparse=}")
+                        # extract json from tarfile 
+                        with tf.extractfile(member) as json_fp:
+                            # read JSON objects in streams
+                            for item in ijson.items(json_fp,prefix="item"):
+                                yield item
+                except Exception:
+                    logger.exception(f"Cannot process TarFile: {fp.name} members")
+        except Exception:
+            logger.exception(f"Cannot Open ZipFile object: {fileinfo}")
 
 #%%
+
 if __name__ == "__main__":
-    
-    args = get_parser().parse_args()
+        
+    # Basic Set up
+    args = get_parser().parse_args(["txtreuse.zip","-v"])
     logging.basicConfig(level=args.loglevel)
     logger =  logging.getLogger(__name__)
+    # set the interpreter to the poerty env
+    os.environ['PYSPARK_PYTHON'] = "/home/jovyan/work/etl_textreuse/.venv/bin/python"
+    spark = (SparkSession
+            .builder
+            .appName("ETL")
+            .getOrCreate())
+    spark.sparkContext.setLogLevel("WARN")
+    sc = spark.sparkContext
+
     if args.local:
         logger.info("Using local file")
         fname = args.fname
         transport_params = dict()
+        output_fname = f"{args.fname.split('.')[0]}.parquet"
     else:
         logger.info("Using S3 file")
+        # load credentials for Pouta s3 storage
         with open(project_root/"s3credentials.toml","r") as fp:
             cred = toml.load(fp)
-
+        # set up credentials for spark
+        sc._jsc.hadoopConfiguration().set("fs.s3a.access.key", cred["default"]["aws_access_key_id"])
+        sc._jsc.hadoopConfiguration().set("fs.s3a.secret.key", cred["default"]["aws_secret_access_key"])
+        sc._jsc.hadoopConfiguration().set("fs.s3a.endpoint", cred["default"]["endpoint_url"])
+        # create a s3 session for boto3
         session = boto3.session.Session()
         s3 = session.client(
             service_name='s3',
             **cred["default"]
         )
-        fname = f"s3://textreuse_raw_data/{args.fname}"
         transport_params=dict(client=s3)
-    # start = time()
-    logger.info("Starting Process")
-    # with a smart open
+        # filename inside the bucket
+        fname = f"s3://{args.s3_bucket}/{args.fname}"
+        # filename inside the bucket
+        output_fname = f"s3a://{args.s3_bucket}/{args.fname.split('.')[0]}.parquet"
+
+    # read the files in the zip file
     with smart_open.open(fname,"rb",compression='disable',transport_params=transport_params) as fileobj:
         # open file as ZIP
         with zipfile.ZipFile(fileobj) as zf:
-            # go over all files in ZIP archive
-            logger.debug("Looping over zip files")
             infolist = zf.infolist()
-            if args.shuffle:
-                random.seed(args.seed)
-                random.shuffle(infolist)
-            for i,fileinfo in enumerate(infolist):
-                # open zip file
-                with zf.open(fileinfo) as fp:
-                    # open tar.gz file
-                    logger.info(f"Opening tar file {fp}")
-                    with tarfile.open(fileobj=fp,debug=3,mode="r|gz") as tf:
-                        # go through members in tarfile
-                        logger.debug("Looping over members")
-                        try:
-                            for member in tf:
-                                logger.debug(f"{member=}")
-                                logger.debug(f"{tf.fileobj=}, {member.offset_data=},{member.size=}, {member.sparse=}")
-                                # extract json from tarfile 
-                                with tf.extractfile(member) as json_fp:
-                                    # read the JSON
-                                    json_str = json_fp.read()
-                                    # # convert it to RDD
-                                    # jsonRDD = sc.parallelize(json_str)
-                                    # # load it as DataFrame
-                                    # https://stackoverflow.com/questions/35770486/spark-streaming-json-to-parquet
-                                    # df = spark.read.json(jsonRDD)
-                                    # # write it out 
-                                    # df.write.parquet(f"{member.name}.parquet")
-                        except Exception:
-                            logger.exception(f"Cannot process TarFile: {fp.name}")
-#%%
+    # get a partial function 
+    func = partial(get_jsons,fname=fname,cred=cred)
+    rdd = sc.parallelize(infolist,args.num_partitions)
+    jsons = rdd.flatMap(func)
+    df = jsons.toDF()
+    df.write.mode("overwrite").parquet(output_fname)
