05ad43f1de404d31c326d4b81ba2456e01a514f0
Ananth Mahadevan <ananth.mahadevan@helsinki.fi>
2023-05-05 11:22:17 +0000

    Adds GO code to cluster pieces using Chinese Whispers

---
+++ cluster_pieces/go.mod
@@ -0,0 +1,44 @@
+module cluster-textreuses
+
+go 1.19
+
+require (
+	github.com/apache/arrow/go/v10 v10.0.1
+	github.com/vbauerster/mpb/v8 v8.1.4
+	github.com/xitongsys/parquet-go v1.6.2
+	github.com/xitongsys/parquet-go-source v0.0.0-20200817004010-026bad9b25d0
+//	lukechampine.com/frand v1.4.2
+)
+
+require (
+	github.com/JohnCGriffin/overflow v0.0.0-20211019200055-46fa312c352c // indirect
+	github.com/VividCortex/ewma v1.2.0 // indirect
+	github.com/acarl005/stripansi v0.0.0-20180116102854-5a71ef0e047d // indirect
+	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/apache/arrow/go/arrow v0.0.0-20200730104253-651201b0f516 // indirect
+	github.com/apache/thrift v0.17.0 // indirect
+	github.com/goccy/go-json v0.9.11 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/google/flatbuffers v2.0.8+incompatible // indirect
+	github.com/klauspost/asmfmt v1.3.2 // indirect
+	github.com/klauspost/compress v1.15.13 // indirect
+	github.com/klauspost/cpuid/v2 v2.1.0 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/minio/asm2plan9s v0.0.0-20200509001527-cdd76441f9d8 // indirect
+	github.com/minio/c2goasm v0.0.0-20190812172519-36a3d3bbc4f3 // indirect
+	github.com/pierrec/lz4/v4 v4.1.15 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/zeebo/xxh3 v1.0.2 // indirect
+	golang.org/x/exp v0.0.0-20220827204233-334a2380cb91 // indirect
+	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
+	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
+	golang.org/x/sync v0.0.0-20220819030929-7fc1605a5dde // indirect
+	golang.org/x/sys v0.0.0-20221013171732-95e765b1cc43 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/tools v0.1.12 // indirect
+	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
+	google.golang.org/genproto v0.0.0-20220401170504-314d38edb7de // indirect
+	google.golang.org/grpc v1.49.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+)
---
+++ cluster_pieces/main.go
@@ -0,0 +1,326 @@
+package main
+
+import (
+	"context"
+	"github.com/apache/arrow/go/v10/arrow"
+	"github.com/apache/arrow/go/v10/arrow/array"
+	"github.com/apache/arrow/go/v10/arrow/memory"
+	"github.com/apache/arrow/go/v10/parquet/file"
+	"github.com/apache/arrow/go/v10/parquet/pqarrow"
+	"github.com/vbauerster/mpb/v8"
+	"github.com/vbauerster/mpb/v8/decor"
+	"github.com/xitongsys/parquet-go-source/local"
+	"github.com/xitongsys/parquet-go/parquet"
+	"github.com/xitongsys/parquet-go/writer"
+	"log"
+	"os"
+	"runtime"
+	"strconv"
+	"sync"
+)
+
+func readTable(filename string) arrow.Table {
+	rdr, err := file.OpenParquetFile(filename, true)
+	if err != nil {
+		log.Println("Can't open file", err)
+		return nil
+	}
+	defer rdr.Close()
+	arrowRdr, err := pqarrow.NewFileReader(rdr, pqarrow.ArrowReadProperties{}, memory.DefaultAllocator)
+	if err != nil {
+		log.Println("Can't open file", err)
+		return nil
+	}
+	tbl, err := arrowRdr.ReadTable(context.Background())
+	if err != nil {
+		log.Println("Can't read file", err)
+		return nil
+	}
+	return tbl
+}
+
+func chunkSlice(slice []int32, chunkCount int) [][]int32 {
+	var chunks [][]int32
+	chunkSize := len(slice)/chunkCount + 1
+	for i := 0; i < len(slice); i += chunkSize {
+		end := i + chunkSize
+
+		// necessary check to avoid slicing beyond
+		// slice capacity
+		if end > len(slice) {
+			end = len(slice)
+		}
+
+		chunks = append(chunks, slice[i:end])
+	}
+	return chunks
+}
+
+type Ret struct {
+	chunkNumber int
+	changed     int
+}
+
+func chineseWhispersSlice(myOrder []int32) int { //, chunkNumber int, r frand.RNG) int {
+	//r.Shuffle(len(myOrder), func(i, j int) { myOrder[i], myOrder[j] = myOrder[j], myOrder[i] })
+	/*	bar := p.AddBar(int64(len(myOrder)),
+		mpb.BarRemoveOnComplete(),
+		mpb.PrependDecorators(
+			decor.Name(fmt.Sprintf("%v: ", chunkNumber)),
+			decor.CountersNoUnit("%d / %d")),
+		mpb.AppendDecorators(
+			decor.Elapsed(decor.ET_STYLE_GO),
+			decor.Name("<"),
+			decor.AverageETA(decor.ET_STYLE_GO),
+			decor.Name(", "),
+			decor.AverageSpeed(0, "%.fnode/s"),
+		),
+	)*/
+	changed := 0
+	//top := make([]int32, 0)
+	for _, e := range myOrder {
+		if !active[e] {
+			//bar.Increment()
+			continue
+		}
+		active[e] = false
+		beg := edgeOffsets[e]
+		end := edgeOffsets[e+1]
+		var nc int32
+		if end == beg+1 {
+			nc = clusters[edgeVals[beg]]
+		} else if end == beg+2 {
+			nc1 := clusters[edgeVals[beg]]
+			nc2 := clusters[edgeVals[beg+1]]
+			if nc1 > nc2 {
+				nc = nc1
+			} else {
+				nc = nc2
+			}
+			/*			if nc1 != nc2 {
+							active[e] = true
+							if r.Intn(2) == 0 {
+								nc = nc1
+							} else {
+								nc = nc2
+							}
+						} else {
+							nc = nc1
+						} */
+		} else {
+			topc := 0
+			cc := make(map[int32]int)
+			for j := beg; j < end; j++ {
+				cc[clusters[edgeVals[j]]]++
+			}
+			/*			if len(cc) == 1 {
+						for cl := range cc {
+							nc = cl
+						}
+					} else {*/
+			for cl, c := range cc {
+				if c > topc {
+					topc = c
+					nc = cl
+					/*						top = top[:0]
+											top = append(top, cl) */
+				} else if c == topc {
+					//						top = append(top, cl)
+					if cl > nc {
+						nc = cl
+					}
+				}
+			}
+			/*				if len(top) > 1 {
+									active[e] = true
+								}
+								nc = top[r.Intn(len(top))]
+			} */
+		}
+		if clusters[e] != nc {
+			clusters[e] = nc
+			for j := beg; j < end; j++ {
+				active[edgeVals[j]] = true
+			}
+			changed++
+		}
+		//bar.Increment()
+	}
+	return changed
+}
+
+func worker(chunkNumbers <-chan int, changed chan<- *Ret, wg *sync.WaitGroup) {
+	var ok bool
+	chunkNumber := -1
+	//r := frand.New()
+	defer wg.Done()
+	for {
+		chunkNumber, ok = <-chunkNumbers
+		if !ok {
+			break
+		}
+		ret := new(Ret)
+		ret.chunkNumber = chunkNumber
+		myOrder := orderChunks[chunkNumber]
+		ret.changed = chineseWhispersSlice(myOrder) //, chunkNumber, *r)
+		changed <- ret
+	}
+}
+
+var p *mpb.Progress
+var clusters []int32
+var edgeOffsets []int32
+var edgeVals []int32
+var orderChunks [][]int32
+var active []bool
+
+type Cluster struct {
+	Cluster int32 `parquet:"name=cluster, type=INT32"`
+}
+
+func writeClusters(numThreads int) {
+	fw, err := local.NewLocalFileWriter("../data/clusters.parquet")
+	if err != nil {
+		log.Println("Can't create local file", err)
+		return
+	}
+
+	//write
+	pw, err := writer.NewParquetWriter(fw, new(Cluster), int64(numThreads))
+	if err != nil {
+		log.Println("Can't create parquet writer", err)
+		return
+	}
+	pw.CompressionType = parquet.CompressionCodec_ZSTD
+	for _, c := range clusters {
+		clu := Cluster{
+			Cluster: c,
+		}
+		if err = pw.Write(clu); err != nil {
+			log.Println("Write error", err)
+		}
+	}
+	if err = pw.WriteStop(); err != nil {
+		log.Println("WriteStop error", err)
+		return
+	}
+	err = fw.Close()
+	if err != nil {
+		log.Println("Close error", err)
+		return
+	}
+}
+
+func main() {
+	numThreads := runtime.GOMAXPROCS(0)
+	log.Println("Reading graph from parquet")
+	edgesTbl := readTable("../data/defrag_graph_adj_list.parquet")
+	log.Println("Graph read")
+	defer edgesTbl.Release()
+	edgesColumn := edgesTbl.Column(0)
+	if len(edgesColumn.Data().Chunks()) != 1 {
+		log.Println("Expected parquet file to only have one chunk")
+		return
+	}
+	edgeLists := edgesColumn.Data().Chunk(0).(*array.List)
+	edgeOffsets = edgeLists.Offsets()
+	edgeVals = edgeLists.ListValues().(*array.Int32).Int32Values()
+	_, err := os.Stat("../data/clusters.parquet")
+	order := make([]int32, edgeLists.Len())
+	active = make([]bool, edgeLists.Len())
+	for i := range active {
+		order[i] = int32(i)
+		active[i] = true
+	}
+	clusters = make([]int32, edgeLists.Len())
+	if os.IsNotExist(err) {
+		log.Println("No saved clusters exist, initializing as new")
+		copy(clusters, order)
+	} else {
+		clc := readTable("../data/clusters.parquet").Column(0).Data().Chunks()
+		log.Println("Reading clusters from saved data")
+		i := 0
+		for _, chunk := range clc {
+			for _, v := range chunk.(*array.Int32).Int32Values() {
+				clusters[i] = v
+				i++
+			}
+		}
+	}
+	/*log.Println("Shuffling nodes")
+	r := frand.New()
+	r.Shuffle(len(order), func(i, j int) { order[i], order[j] = order[j], order[i] })*/
+	var wg sync.WaitGroup
+	p = mpb.New(mpb.WithWaitGroup(&wg))
+	maxIters := 100000
+	lastChanged := 0
+	log.Println("Doing chinese whispers")
+	bar := p.AddBar(int64(maxIters),
+		mpb.PrependDecorators(
+			decor.CountersNoUnit("%d / %d, changed: "),
+			decor.Any(func(st decor.Statistics) string {
+				return strconv.FormatInt(int64(lastChanged), 10)
+			}),
+		),
+		mpb.AppendDecorators(
+			decor.Elapsed(decor.ET_STYLE_GO),
+			decor.Name("<"),
+			decor.AverageETA(decor.ET_STYLE_GO),
+			decor.Name(", "),
+			decor.AverageSpeed(0, "%.2f it/s"),
+		))
+	orderChunks = chunkSlice(order, numThreads)
+	chunkChan := make(chan int, numThreads)
+	changedChan := make(chan *Ret, numThreads)
+	defer close(changedChan)
+	wg.Add(numThreads)
+	for i := 0; i < numThreads; i++ {
+		go worker(chunkChan, changedChan, &wg)
+	}
+	for j := range orderChunks {
+		chunkChan <- j
+	}
+	iters := 0
+	currentChanged := 0
+	for {
+		ret := <-changedChan
+		currentChanged += ret.changed
+		iters++
+		if iters%numThreads == 0 {
+			lastChanged = currentChanged
+			bar.Increment()
+			/*if iters%(numThreads*100) == 0 {
+				writeClusters(numThreads)
+			}*/
+			if iters == maxIters || currentChanged == 0 {
+				close(chunkChan)
+				break
+			}
+			currentChanged = 0
+		}
+		chunkChan <- ret.chunkNumber
+	}
+	if iters < maxIters {
+		/*
+			for i := range active {
+				active[i] = true
+			}*/
+		for {
+			lastChanged = chineseWhispersSlice(order) //, -1, *r)
+			bar.Increment()
+			iters++
+			if iters == maxIters {
+				break
+			}
+			if lastChanged == 0 {
+				bar.IncrInt64(int64(maxIters) - bar.Current())
+				break
+			}
+		}
+	}
+	p.Wait()
+	log.Printf("Done after %v iterations\n", iters)
+	log.Println("Writing clusters")
+	writeClusters(numThreads)
+	log.Println("Write Finished")
+}
