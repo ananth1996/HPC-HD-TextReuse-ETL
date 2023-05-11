a2057507bd02af232435a098e002776a4e154a89
Ananth Mahadevan <ananth.mahadevan@helsinki.fi>
2023-05-11 19:33:12 +0000

    Refactors and orders tables

--- unique_ids.py
+++ unique_ids.py
@@ -11,37 +11,49 @@ textreuses_raw = get_s3(fname="txtreuse",bucket=raw_bucket,table_name="textreuse
 textreuse_ids = materialise_row_numbers(
     fname = "textreuse_ids",
     df = spark.sql("""   
+    SELECT * FROM (
         SELECT 
             text1_id AS text_name,
-            SUBSTRING_INDEX(text1_id,".",1) AS doc_name,
-            SUBSTRING_INDEX(text1_id,".",-1) AS struct_name
+            SUBSTRING_INDEX(text1_id ,".",1) as manifestation_id,
+            (CASE 
+		        WHEN LOCATE(".",text1_id) > 0 
+                THEN SUBSTRING_INDEX(text1_id ,".",-1)
+		        ELSE NULL
+	        END) AS structure_name
         FROM textreuses_raw
         UNION
         SELECT 
             text2_id AS text_name, 
-            SUBSTRING_INDEX(text2_id,".",1) AS doc_name,
-            SUBSTRING_INDEX(text2_id,".",-1) AS struct_name 
+            SUBSTRING_INDEX(text2_id,".",1) AS manifestation_id,
+            (CASE 
+		        WHEN LOCATE(".",text2_id) > 0 
+                THEN SUBSTRING_INDEX(text2_id ,".",-1)
+		        ELSE NULL
+	        END) AS structure_name
         FROM textreuses_raw
-        """),
-    col_name="textreuse_source_id",
-    bucket = args.output_s3_bucket)
+    ) 
+    ORDER BY manifestation_id,structure_name"""),
+    col_name="trs_id",
+    bucket = processed_bucket)
 #%%
 textreuses = materialise_row_numbers(
     fname="textreuses",
     df=spark.sql("""
         SELECT 
-            ti1.textreuse_source_id AS trs1_id,
+            ti1.trs_id AS trs1_id,
             text1_text_start AS trs1_start,
             text1_text_end AS trs1_end,
-            ti2.textreuse_source_id AS trs2_id,
+            ti2.trs_id AS trs2_id,
             text2_text_start AS trs2_start,
             text2_text_end AS trs2_end,
             align_length,
             positives_percent
         FROM textreuses_raw t
         LEFT JOIN textreuse_ids ti1 ON t.text1_id = ti1.text_name
-        LEFT JOIN textreuse_ids ti2 ON t.text2_id = ti2.text_name"""),
+        LEFT JOIN textreuse_ids ti2 ON t.text2_id = ti2.text_name
+        ORDER BY trs1_id,trs2_id
+        """),
     col_name="textreuse_id",
-    bucket=args.output_s3_bucket
+    bucket=processed_bucket
 )
 #%%
