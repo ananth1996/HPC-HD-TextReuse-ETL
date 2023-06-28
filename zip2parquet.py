68fcfd0332befb2fa34710f0238883ac91688822
Ananth Mahadevan <ananth.mahadevan@helsinki.fi>
2023-06-28 14:49:50 +0000

    Updates zip2parquet for new data

--- zip2parquet.py
+++ zip2parquet.py
@@ -12,14 +12,14 @@ import os
 import ijson 
 from functools import partial
 from pyspark.sql.types import *
+from spark_utils import *
 project_root = Path(__file__).parent.resolve()
+import json
 #%% 
 
 def get_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("fname",help="Name of the file to process",type=str)
-    parser.add_argument("--local",help="Whether to use local file",action="store_true")
-    parser.add_argument("--s3_bucket",type=str,help="The s3 bucket where zip file is stored",default="textreuse-raw-data")
     parser.add_argument("--num_partitions",type=int,default=200,help="Number of partitions for spark")
     parser.add_argument(
     '-d', '--debug',
@@ -45,27 +45,16 @@ def process_file(fileinfo:zipfile.ZipInfo,handler:zipfile.ZipFile):
         dict: JSON objects
     """
 
-    with handler.open(fileinfo) as fp:
+    with handler.open(fileinfo) as json_fp:
         try:
-            # open tar.gz file
-            logger.info(f"Opening tar file {fp}")
-            with tarfile.open(fileobj=fp,debug=3,mode="r|gz") as tf:
-                # go through members in tarfile
-                logger.debug("Looping over members")
-                try:
-                    for member in tf:
-                        logger.debug(f"{member=}")
-                        logger.debug(f"{tf.fileobj=}, {member.offset_data=},{member.size=}, {member.sparse=}")
-                        # extract json from tarfile 
-                        counter = 0
-                        with tf.extractfile(member) as json_fp:
-                            # read JSON objects in streams
-                            for item in ijson.items(json_fp,prefix="item",use_float=True):
-                                counter+=1 
-                                logger.debug(f"Returning item: {counter}")
-                                yield item
-                except Exception:
-                    logger.exception(f"Cannot process TarFile: {fp.name}")
+            logger.info(f"Opening json file: {json_fp}")
+            counter = 0
+            # read JSON objects in streams
+            for line in json_fp:
+                counter+=1 
+                logger.debug(f"Returning item: {counter}")
+                item = json.loads(line)
+                yield item
         except Exception:
             logger.exception(f"Cannot Open ZipFile object: {fileinfo}")
     
@@ -106,43 +95,25 @@ def process_partition(iterator,fname:str,cred:dict,loglevel:int):
 if __name__ == "__main__":
         
     # Basic Set up
-    args = get_parser().parse_args(["txtreuse.zip"])
+    args = get_parser().parse_args(["tr_data_out_all.zip"])
     logging.basicConfig(level=args.loglevel)
     logger =  logging.getLogger(__name__)
-    # set the interpreter to the poerty env
-    os.environ['PYSPARK_PYTHON'] = str(project_root/".venv/bin/python")
-    spark = (SparkSession
-            .builder
-            .appName("ETL")
-            .getOrCreate())
-    spark.sparkContext.setLogLevel("WARN")
-    sc = spark.sparkContext
 
-    if args.local:
-        logger.info("Using local file")
-        fname = args.fname
-        transport_params = dict()
-        output_fname = f"{args.fname.split('.')[0]}.parquet"
-    else:
-        logger.info("Using S3 file")
-        # load credentials for Pouta s3 storage
-        with open(project_root/"s3credentials.toml","r") as fp:
-            cred = toml.load(fp)
-        # set up credentials for spark
-        sc._jsc.hadoopConfiguration().set("fs.s3a.access.key", cred["default"]["aws_access_key_id"])
-        sc._jsc.hadoopConfiguration().set("fs.s3a.secret.key", cred["default"]["aws_secret_access_key"])
-        sc._jsc.hadoopConfiguration().set("fs.s3a.endpoint", cred["default"]["endpoint_url"])
-        # create a s3 session for boto3
-        session = boto3.session.Session()
-        s3 = session.client(
-            service_name='s3',
-            **cred["default"]
-        )
-        transport_params=dict(client=s3)
-        # filename inside the bucket
-        fname = f"s3://{args.s3_bucket}/{args.fname}"
-        # filename inside the bucket
-        output_fname = f"s3a://{args.s3_bucket}/{args.fname.split('.')[0]}.parquet"
+    logger.info("Using S3 file")
+    # load credentials for Pouta s3 storage
+    with open(project_root/"s3credentials.toml","r") as fp:
+        cred = toml.load(fp)
+    
+    session = boto3.session.Session()
+    s3 = session.client(
+        service_name='s3',
+        **cred["default"]
+    )
+    transport_params=dict(client=s3)
+    # filename inside the bucket
+    fname = f"s3://{raw_bucket}/{args.fname}"
+    # filename inside the bucket
+    output_fname = f"s3a://{raw_bucket}/{args.fname.split('.')[0]}.parquet"
     #%%
     # read the files in the zip file
     with smart_open.open(fname,"rb",compression='disable',transport_params=transport_params) as fileobj:
