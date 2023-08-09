37fce821bc4f63a39d6945a45574b94693feea52
Ananth Mahadevan <ananth.mahadevan@helsinki.fi>
2023-08-09 13:01:55 +0300

    Fixes date parsing issue with newspapers meta
    There were dates like 1732-00-00 which failed parse previously as NULLs.

--- downstream.py
+++ downstream.py
@@ -22,13 +22,23 @@ if s3_uri_exists(newspapers_raw_parquet,raw_bucket):
     newspapers_core = get_s3("newspapers_core",raw_bucket)
 else:
     newspapers_core = spark.read.option("header",True).csv(newspapers_raw_csv)
-    newspapers_core = (newspapers_core
-                    .withColumn("issue_date_start",to_date(col("issue_date_start"),"yyyy-MM-dd"))
-                    .withColumn("issue_date_end",to_date(col("issue_date_end"),"yyyy-MM-dd"))
-                    )
+    newspapers_core.createOrReplaceTempView("newspapers_core")
     newspapers_core = materialise_s3(
         fname="newspapers_core",
-        df=newspapers_core,
+        df=spark.sql("""
+        SELECT *,
+        (CASE 
+        WHEN to_date(issue_date_start,'yyyy-MM-dd') IS NULL -- when str is '1732-00-00'
+        THEN to_date(SUBSTRING(issue_date_start,1,4),'yyyy') '1732-00-00' -> '1732' -> 1732-01-01
+        ELSE to_date(issue_date_start,'yyyy-MM-dd')
+        END) AS issue_start_date,
+        (CASE 
+        WHEN to_date(issue_date_end,'yyyy-MM-dd') IS NULL 
+        THEN to_date(SUBSTRING(issue_date_end,1,4),'yyyy')
+        ELSE to_date(issue_date_end,'yyyy-MM-dd')
+        END) AS issue_end_date
+        FROM newspapers_core
+        """),
         bucket=raw_bucket
     )
 #%%
