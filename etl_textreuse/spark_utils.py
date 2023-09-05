3a883eb50e0a0e9250db2c4f92241f7b49443af1
Ananth Mahadevan <ananth.mahadevan@helsinki.fi>
2023-09-05 18:21:42 +0300

    Modifies spark utils to have explicit SparkSession

--- etl_textreuse/spark_utils.py
+++ etl_textreuse/spark_utils.py
@@ -64,178 +64,190 @@ def get_spark_session(project_root:Path,application_name:str="ETL"):
 # # create a mapping for easy access
 # fs_dict = {processed_bucket:processed_fs,raw_bucket:raw_fs,denorm_bucket:denorm_fs}
 
-# def get_local(fname: str, cache: bool = False,table_name: Optional[str] = None):
-#     if table_name is None:
-#         table_name = fname
-#     return register(table_name, spark.read.parquet(fname+".parquet"), cache=cache)
+def get_local(spark:SparkSession,fname: str, cache: bool = False,table_name: Optional[str] = None):
+    if table_name is None:
+        table_name = fname
+    return register(spark, table_name, spark.read.parquet(fname+".parquet"), cache=cache)
     
     
-# def materialise_local(name:str, df, cache: bool = False):
-#     df.write.mode("overwrite").option("compression","zstd").parquet(name+".parquet")
-#     return get_local(name, cache)
+def materialise_local(spark:SparkSession, name:str, df, cache: bool = False):
+    df.write.mode("overwrite").option("compression","zstd").parquet(name+".parquet")
+    return get_local(spark, name, cache)
 
-# def register(table_name: str, df, cache: bool):
-#     if cache:
-#         df.createOrReplaceTempView(table_name + "_source")
-#         spark.sql("DROP TABLE IF EXISTS " + table_name)
-#         spark.sql("CACHE TABLE " + table_name + " AS TABLE " + table_name + "_source")
-#         return spark.sql("SELECT * FROM " + table_name)
-#     else:
-#         df.createOrReplaceTempView(table_name)
-#         return df
+def register(spark: SparkSession, table_name: str, df, cache: bool):
+    if cache:
+        df.createOrReplaceTempView(table_name + "_source")
+        spark.sql("DROP TABLE IF EXISTS " + table_name)
+        spark.sql("CACHE TABLE " + table_name + " AS TABLE " + table_name + "_source")
+        return spark.sql("SELECT * FROM " + table_name)
+    else:
+        df.createOrReplaceTempView(table_name)
+        return df
 
-# def s3_uri_exists(path: str,bucket:Optional[str]=None):
-#     if bucket is None:
-#         # create a file system for that bucket
-#         fs = sc._jvm.org.apache.hadoop.fs.FileSystem.get(
-#             sc._jvm.java.net.URI.create(path), sc._jsc.hadoopConfiguration()
-#         )
-#     else:
-#         fs = fs_dict[bucket]
-#     return fs.exists(sc._jvm.org.apache.hadoop.fs.Path(path))
+def get_hadoop_filesystem(spark:SparkSession,path:str):
+    sc = spark.sparkContext
+    # create a file system for that bucket
+    fs = sc._jvm.org.apache.hadoop.fs.FileSystem.get(
+        sc._jvm.java.net.URI.create(path), sc._jsc.hadoopConfiguration()
+    )
+    return fs
 
-# def get_s3(
-#     fname: str,
-#     bucket: str,
-#     cache: bool = False,
-#     table_name: Optional[str] = None,
-# ):
-#     if table_name is None:
-#         table_name = fname
-#     return register(
-#         table_name, spark.read.parquet(f"s3a://{bucket}/{fname}.parquet"), cache=cache
-#     )
+def get_hadoop_path(spark:SparkSession,path:str):
+    return spark.sparkContext._jvm.org.apache.hadoop.fs.Path(path)
 
-# def delete_s3(
-#         fname:str,
-#         bucket:str,
-# ):
-#     path = f"s3a://{bucket}/{fname}.parquet"
-#     _path = sc._jvm.org.apache.hadoop.fs.Path(path)
-#     fs = fs_dict[bucket]
-#     if fs.exists(_path):
-#         fs.delete(_path,True)
+def s3_uri_exists(spark:SparkSession, path: str):
+    fs = get_hadoop_filesystem(spark,path)    
+    return fs.exists(get_hadoop_path(spark,path))
 
-# def rename_s3(src_fname:str,dst_fname:str,bucket:str):
-#     _src_path = sc._jvm.org.apache.hadoop.fs.Path(f"s3a://{bucket}/{src_fname}.parquet")
-#     _dst_path = sc._jvm.org.apache.hadoop.fs.Path(f"s3a://{bucket}/{dst_fname}.parquet")
-#     fs = fs_dict[bucket]
-#     if (fs.exists(_src_path)):
-#         fs.rename(_src_path,_dst_path)
+def get_s3(
+    spark: SparkSession,
+    fname: str,
+    bucket: str,
+    cache: bool = False,
+    table_name: Optional[str] = None,
+):
+    if table_name is None:
+        table_name = fname
+    return register(
+        spark, table_name, spark.read.parquet(f"s3a://{bucket}/{fname}.parquet"), cache=cache
+    )
 
-# def materialise_s3(
-#     fname: str,
-#     df: DataFrame,
-#     bucket: str,
-#     cache: bool = False,
-#     table_name: Optional[str] = None,
-# ):
-#     df.write.mode("overwrite").parquet(f"s3a://{bucket}/{fname}.parquet")
-#     return get_s3(fname, bucket, cache, table_name)
+def delete_s3(
+        spark: SparkSession,
+        fname:str,
+        bucket:str,
+):
+    path = f"s3a://{bucket}/{fname}.parquet"
+    fs = get_hadoop_filesystem(spark,path)
+    _path = get_hadoop_path(path)
+    if fs.exists(_path):
+        fs.delete(_path,True)
+
+def rename_s3(spark:SparkSession,src_fname:str,dst_fname:str,bucket:str):
+    _src_path = get_hadoop_path(spark,f"s3a://{bucket}/{src_fname}.parquet")
+    _dst_path = get_hadoop_path(spark,f"s3a://{bucket}/{dst_fname}.parquet")
+    fs = get_hadoop_filesystem(spark,f"s3a://{bucket}/{src_fname}.parquet")
+    if (fs.exists(_src_path)):
+        fs.rename(_src_path,_dst_path)
+
+def materialise_s3(
+    spark:SparkSession,
+    fname: str,
+    df: DataFrame,
+    bucket: str,
+    cache: bool = False,
+    table_name: Optional[str] = None,
+):
+    df.write.mode("overwrite").parquet(f"s3a://{bucket}/{fname}.parquet")
+    return get_s3(spark, fname, bucket, cache, table_name)
 
 
-# def materialise_s3_if_not_exists(
-#     fname: str,
-#     df: DataFrame,
-#     bucket: str,
-#     cache: bool = False,
-#     table_name: Optional[str] = None,
-# ):
-#     if not s3_uri_exists(f"s3a://{bucket}/{fname}.parquet"):
-#         return materialise_s3(fname, df, bucket, cache, table_name)
-#     else:
-#         return get_s3(fname, bucket, cache, table_name)
+def materialise_s3_if_not_exists(
+    spark:SparkSession,
+    fname: str,
+    df: DataFrame,
+    bucket: str,
+    cache: bool = False,
+    table_name: Optional[str] = None,
+):
+    if not s3_uri_exists(spark,f"s3a://{bucket}/{fname}.parquet"):
+        return materialise_s3(spark,fname, df, bucket, cache, table_name)
+    else:
+        return get_s3(spark,fname, bucket, cache, table_name)
     
-# #%%
-# # From https://stackoverflow.com/questions/30304810/dataframe-ified-zipwithindex
-# def dfZipWithIndex (df, offset=1, col_name="rowId"):
-#     '''
-#         Enumerates dataframe rows is native order, like rdd.ZipWithIndex(), but on a dataframe
-#         and preserves a schema
+#%%
+# From https://stackoverflow.com/questions/30304810/dataframe-ified-zipwithindex
+def dfZipWithIndex (spark:SparkSession, df, offset=1, col_name="rowId"):
+    '''
+        Enumerates dataframe rows is native order, like rdd.ZipWithIndex(), but on a dataframe
+        and preserves a schema
 
-#         :param df: source dataframe
-#         :param offset: adjustment to zipWithIndex()'s index
-#         :param colName: name of the index column
-#     '''
+        :param df: source dataframe
+        :param offset: adjustment to zipWithIndex()'s index
+        :param colName: name of the index column
+    '''
 
-#     new_schema = StructType(
-#                     [StructField(col_name,LongType(),True)]        # new added field in front
-#                     + df.schema.fields                            # previous schema
-#                 )
+    new_schema = StructType(
+                    [StructField(col_name,LongType(),True)]        # new added field in front
+                    + df.schema.fields                            # previous schema
+                )
 
-#     zipped_rdd = df.rdd.zipWithIndex()
-#     # use this for python 3+, tuple gets passed as single argument so using args and [] notation to read elements within args
-#     new_rdd = zipped_rdd.map(lambda args: ([args[1] + offset] + list(args[0])))      
-#     return spark.createDataFrame(new_rdd, new_schema)
-
-
-# def materialise_row_numbers(fname:str,df:DataFrame,col_name:str,bucket:str,table_name:Optional[str]=None):
-#     # check if row numbers already materialised
-#     if s3_uri_exists(f"s3a://{bucket}/{fname}.parquet",bucket=bucket):
-#         _df = get_s3(fname,bucket=bucket)
-#     else:
-#         # write with row numbers to the location
-#         _df = materialise_s3(
-#             fname=fname,
-#             df=dfZipWithIndex(df,col_name=col_name),
-#             bucket=bucket
-#         )
-#     return _df
+    zipped_rdd = df.rdd.zipWithIndex()
+    # use this for python 3+, tuple gets passed as single argument so using args and [] notation to read elements within args
+    new_rdd = zipped_rdd.map(lambda args: ([args[1] + offset] + list(args[0])))      
+    return spark.createDataFrame(new_rdd, new_schema)
 
 
-# def materialise_with_int_id(fname:str,df:DataFrame,col_name:str,id_col_name:str,bucket:str,keep_id_mapping:bool=True,id_fname:Optional[str]=None,drop_col:bool=True) -> DataFrame:
-#     """Creates INT ids for a column in dataframe and adds it back as id columns
+def materialise_row_numbers(spark:SparkSession,fname:str,df:DataFrame,col_name:str,bucket:str,table_name:Optional[str]=None):
+    # check if row numbers already materialised
+    # if s3_uri_exists(spark,f"s3a://{bucket}/{fname}.parquet"):
+    #     _df = get_s3(spark,fname,bucket=bucket)
+    # else:
+    # write with row numbers to the location
+    _df = materialise_s3(
+        spark,
+        fname=fname,
+        df=dfZipWithIndex(spark,df,col_name=col_name),
+        bucket=bucket
+    )
+    return _df
 
-#     Args:
-#         fname (str): The filename to store the dataframe in
-#         df (DataFrame): The dataframe 
-#         col_name (str): The column for which to create integer IDS
-#         id_col_name (str): The column name for the new ID column
-#         bucket (str): The bucket where to store the data
-#         keep_id_mapping(bool): Whether to keep the INT id mapping file. Defaults to True
-#         id_fname(str): Optional. The name of id mapping file. Defaults to None.
-#         drop_col(bool): Optional. Whether to drop col_name from resulting dataframe. Defaults to True.
 
-#     Returns:
-#         DataFrame: The dataframe with a new column with INT ids
-#     """
-#     if s3_uri_exists(f"s3a://{bucket}/{fname}.parquet"):
-#         rename_s3(fname,fname,bucket=bucket)
-#         # load dataframe 
-#         _df = get_s3(fname,bucket=bucket)
-#     else:
-#         register(fname,df,False)
-#         if id_fname is None:
-#             id_fname = col_name+"_id_mapping"
-        
-#         # make INT ids for column
-#         id_df = materialise_row_numbers(
-#             fname=id_fname,
-#             df=spark.sql(f"""
-#                 SELECT DISTINCT {col_name}
-#                 FROM {fname}
-#                 ORDER BY {col_name}
-#                 """),
-#             col_name=id_col_name,
-#             bucket=bucket
-#             )
-#         # update the dataframe with the new mapping
-#         query =spark.sql(f"""
-#                 SELECT orig.*,{id_col_name} FROM {fname} orig
-#                 INNER JOIN {id_fname} USING ({col_name})
-#                 """)
-#         if drop_col:
-#             query=query.drop(col_name) 
-#         # materialise the dataframe
-#         _df = materialise_s3(
-#             fname=fname, 
-#             df=query,
-#             bucket=bucket
-#             )
-#         if not keep_id_mapping:
-#             delete_s3(id_fname,bucket=bucket)
+def materialise_with_int_id(spark:SparkSession,fname:str,df:DataFrame,col_name:str,id_col_name:str,bucket:str,keep_id_mapping:bool=True,id_fname:Optional[str]=None,drop_col:bool=True) -> DataFrame:
+    """Creates INT ids for a column in dataframe and adds it back as id columns
+
+    Args:
+        fname (str): The filename to store the dataframe in
+        df (DataFrame): The dataframe 
+        col_name (str): The column for which to create integer IDS
+        id_col_name (str): The column name for the new ID column
+        bucket (str): The bucket where to store the data
+        keep_id_mapping(bool): Whether to keep the INT id mapping file. Defaults to True
+        id_fname(str): Optional. The name of id mapping file. Defaults to None.
+        drop_col(bool): Optional. Whether to drop col_name from resulting dataframe. Defaults to True.
+
+    Returns:
+        DataFrame: The dataframe with a new column with INT ids
+    """
+    # if s3_uri_exists(spark,f"s3a://{bucket}/{fname}.parquet"):
+    #     rename_s3(spark,fname,fname,bucket=bucket)
+    #     # load dataframe 
+    #     _df = get_s3(spark,fname,bucket=bucket)
+    # else:
+    register(spark,fname,df,False)
+    if id_fname is None:
+        id_fname = col_name+"_id_mapping"
     
-#     return _df
+    # make INT ids for column
+    id_df = materialise_row_numbers(
+        spark,
+        fname=id_fname,
+        df=spark.sql(f"""
+            SELECT DISTINCT {col_name}
+            FROM {fname}
+            ORDER BY {col_name}
+            """),
+        col_name=id_col_name,
+        bucket=bucket
+        )
+    # update the dataframe with the new mapping
+    query =spark.sql(f"""
+            SELECT orig.*,{id_col_name} FROM {fname} orig
+            INNER JOIN {id_fname} USING ({col_name})
+            """)
+    if drop_col:
+        query=query.drop(col_name) 
+    # materialise the dataframe
+    _df = materialise_s3(
+        spark,
+        fname=fname, 
+        df=query,
+        bucket=bucket
+        )
+    if not keep_id_mapping:
+        delete_s3(spark,id_fname,bucket=bucket)
+
+    return _df
 
 # with open(project_root/"database.toml") as fp:
 #     db_options = toml.load(fp)
