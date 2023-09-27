994cf47d193d6d8aecd8ad4362dd1802108906ce
Ananth Mahadevan <ananth.mahadevan@helsinki.fi>
2023-09-27 10:01:50 +0300

    Adds SQLAlchemy engine creation

--- db_utils.py
+++ db_utils.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 import toml
 from pathlib import Path
+from sqlalchemy.pool import NullPool
 project_root = Path(__file__).parent.resolve()
 
 with open(project_root/"database.toml") as fp:
@@ -10,6 +11,12 @@ with open(project_root/"database.toml") as fp:
 def get_sqlalchemy_connect(version):
     opts = db_options[version]
     engine_string = f"mysql+pymysql://{opts['user']}:{opts['password']}@{opts['host']}/{opts['database']}?charset=utf8mb4"
-    engine = create_engine(engine_string, future=True)
+    engine = create_engine(engine_string, future=True, poolclass=NullPool)
     conn = engine.connect()
     return conn
+
+def get_sqlalchemy_engine(version):
+    opts = db_options[version]
+    engine_string = f"mysql+pymysql://{opts['user']}:{opts['password']}@{opts['host']}/{opts['database']}?charset=utf8mb4"
+    engine = create_engine(engine_string, future=True, poolclass=NullPool)
+    return engine
