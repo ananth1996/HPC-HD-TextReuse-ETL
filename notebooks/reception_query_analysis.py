c44182ea1795268dd3d882bbb74ed7f11aea1182
Ananth Mahadevan <ananth.mahadevan@helsinki.fi>
2023-10-11 13:35:57 +0000

    Updates plotting notebook

--- notebooks/reception_query_analysis.py
+++ notebooks/reception_query_analysis.py
@@ -18,26 +18,18 @@ import numpy as np
 import powerlaw
 #%%
 data_dir = project_root/"data"
-num_reception_edges = pd.read_csv(data_dir/"num_reception_edges.csv")
-reception_query_results = pd.read_csv(data_dir/"reception_query_results.csv")
+samples = []
+databases = ["hpc-hd","hpc-hd-newspapers"]
+for database in databases:
+    _df = pd.read_csv(data_dir/f"{database}-samples.csv")
+    _df["database"]=database
+    samples.append(_df)
+
+samples = pd.concat(samples)
+
+results = pd.read_csv(data_dir/"reception-queries-results-1.csv")
+results = results.merge(samples,left_on=["doc_id","database"],right_on=["manifestation_id","database"])
 #%%[markdown]
-### Distribution of number of reception edges
-#%%
-bin_centres,hist = log_binning(num_reception_edges.num_reception_edges,density=False)
-plt.loglog(bin_centres,hist)
-plt.xlabel("Number of Reception Edges")
-plt.ylabel("Number of documents")
-plt.title("Power Law distribution")
-# %%
-reception_query_results
-# %%
-melt_df = reception_query_results.melt(id_vars=["trs_id","quartile"],value_vars=["denorm_time","standard_time"])
-sns.barplot(data=melt_df,x="quartile",y='value',hue="variable")
-plt.ylabel("Running Time in seconds")
-plt.legend(bbox_to_anchor=(1,0.5),loc="upper left")
-plt.title("Spark Results")
-# %%
-sns.barplot(data=reception_query_results,x="quartile",y="ground_truth",log=True)
-plt.ylabel("Number of reception edges")
-plt.title("Distribution of samples")
+sns.catplot(x="query_dists_id", y="duration", hue="query_type", col="database", data=results, kind="bar",log=True)
+plt.ylabel("Query Bucket Number ")
 #%%
