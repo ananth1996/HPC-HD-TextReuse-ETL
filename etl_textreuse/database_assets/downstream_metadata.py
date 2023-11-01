d090ff19d75340f6a745ec329af08cd04a473e5a
Ananth Mahadevan <ananth.mahadevan@helsinki.fi>
2023-11-01 17:53:17 +0200

    Adds manifestation_title database asset

--- etl_textreuse/database_assets/downstream_metadata.py
+++ etl_textreuse/database_assets/downstream_metadata.py
@@ -1,4 +1,4 @@
-from etl_textreuse.spark_utils import get_spark_session,processed_bucket,get_s3,jdbc_opts
+from etl_textreuse.spark_utils import get_spark_session,load_table,processed_bucket
 from etl_textreuse.database_utils import get_sqlalchemy_engine
 from sqlalchemy import text
 from dagster import asset
@@ -23,6 +23,24 @@ def db_manifestation_publication_date() -> None:
     """
     load_table(spark,table,processed_bucket,database,schema,index)
 
+@asset(
+    deps=["manifestation_title"],
+    group_name="database"
+)
+def db_manifestation_title() -> None:
+    spark = get_spark_session(application_name="Load MariaDB")
+    table = "manifestation_title"
+    database = "hpc-hd-newspapers"
+    schema = """
+    CREATE TABLE IF NOT EXISTS `manifestation_title`(
+        `manifestation_id_i` int(11) unsigned NOT NULL,
+        `title` text DEFAULT NULL
+    )ENGINE=Aria PAGE_CHECKSUM=0 TRANSACTIONAL=0;"""
+    index = """
+    ALTER TABLE `manifestation_title`
+    ADD PRIMARY KEY (`manifestation_id_i`);
+    """
+    load_table(spark,table,processed_bucket,database,schema,index)
 
 
         
