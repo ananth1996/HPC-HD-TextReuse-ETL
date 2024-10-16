f1a4f7e13267c26e2571c40c5d468d074e995528
Ananth Mahadevan <ananth.mahadevan@helsinki.fi>
2024-10-16 18:05:32 +0300

    Fixes database connection env variable

--- etl_textreuse/database_utils.py
+++ etl_textreuse/database_utils.py
@@ -10,6 +10,6 @@ def get_sqlalchemy_connect(version):
     return conn
 
 def get_sqlalchemy_engine():
-    engine_string = f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_DATABASE')}?charset=utf8mb4"
+    engine_string = f"mysql+pymysql://{os.getenv('DB_USERNAME')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_DATABASE')}?charset=utf8mb4"
     engine = create_engine(engine_string, future=True, poolclass=NullPool, pool_pre_ping=True)
     return engine
