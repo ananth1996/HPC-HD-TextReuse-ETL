e9c6c466e62040fe7a425aff0d462ff1318777c9
Ananth Mahadevan <ananth.mahadevan@helsinki.fi>
2024-10-16 17:47:03 +0300

    Adds more downstream metadata database assets

--- etl_textreuse/database_assets/downstream_metadata.py
+++ etl_textreuse/database_assets/downstream_metadata.py
@@ -2,7 +2,7 @@ from etl_textreuse.spark_utils import get_spark_session,load_table,processed_buc
 from etl_textreuse.database_utils import get_sqlalchemy_engine
 from sqlalchemy import text
 from dagster import asset, Output, MetadataValue
-
+import os
 
 @asset(
     deps=["manifestation_publication_date"],
@@ -11,7 +11,7 @@ from dagster import asset, Output, MetadataValue
 def db_manifestation_publication_date() -> Output[None]:
     spark = get_spark_session(application_name="Load MariaDB")
     table = "manifestation_publication_date"
-    database = "hpc-hd-newspapers"
+    database = os.getenv('DB_DATABASE')
     schema = """
     CREATE TABLE IF NOT EXISTS `manifestation_publication_date`(
         `manifestation_id_i` int(11) unsigned NOT NULL,
@@ -23,7 +23,49 @@ def db_manifestation_publication_date() -> Output[None]:
     """
     metadata = load_table(spark,table,processed_bucket,database,schema,index)
     return Output(None,metadata=metadata)
+    return Output(None,metadata=metadata)
 
+@asset(
+    deps=["edition_publication_date"],
+    group_name="database"
+)
+def db_edition_publication_date() -> Output[None]:
+    spark = get_spark_session(application_name="Load MariaDB")
+    table = "edition_publication_date"
+    database = os.getenv('DB_DATABASE')
+    schema = """
+    CREATE TABLE IF NOT EXISTS `edition_publication_date`(
+        `edition_id_i` int(11) unsigned NOT NULL,
+        `publication_date` date DEFAULT NULL
+    )ENGINE=Aria PAGE_CHECKSUM=0 TRANSACTIONAL=0;"""
+    index = """
+    ALTER TABLE `edition_publication_date`
+        -- There might be editions with several possible publication dates
+        ADD INDEX IF NOT EXISTS `edition_covering` (`edition_id_i`,`publication_date`);
+    """
+    metadata = load_table(spark,table,processed_bucket,database,schema,index)
+    return Output(None,metadata=metadata)
+
+@asset(
+    deps=["work_earliest_publication_date"],
+    group_name="database"
+)
+def db_work_earliest_publication_date() -> Output[None]:
+    spark = get_spark_session(application_name="Load MariaDB")
+    table = "work_earliest_publication_date"
+    database = os.getenv('DB_DATABASE')
+    schema = """
+    CREATE TABLE IF NOT EXISTS `work_earliest_publication_date` (
+        `work_id_i` int(11) unsigned NOT NULL,
+        `publication_date` date DEFAULT NULL
+    )ENGINE=Aria PAGE_CHECKSUM=0 TRANSACTIONAL=0;
+    """
+    index = """
+    ALTER TABLE `work_earliest_publication_date`
+        ADD PRIMARY KEY (`work_id_i`);
+    """
+    metadata = load_table(spark,table,processed_bucket,database,schema,index)
+    return Output(None,metadata=metadata)
 
 @asset(
     deps=["manifestation_title"],
@@ -32,7 +74,7 @@ def db_manifestation_publication_date() -> Output[None]:
 def db_manifestation_title() -> None:
     spark = get_spark_session(application_name="Load MariaDB")
     table = "manifestation_title"
-    database = "hpc-hd-newspapers"
+    database = os.getenv('DB_DATABASE')
     schema = """
     CREATE TABLE IF NOT EXISTS `manifestation_title`(
         `manifestation_id_i` int(11) unsigned NOT NULL,
@@ -44,5 +86,68 @@ def db_manifestation_title() -> None:
     """
     load_table(spark,table,processed_bucket,database,schema,index)
 
+@asset(
+    deps=["actor_ids"],
+    group_name="database"
+)
+def db_actor_ids() -> Output[None]:
+    spark = get_spark_session(application_name="Load MariaDB")
+    table = "actor_ids"
+    database = os.getenv('DB_DATABASE')
+    schema = """
+    CREATE TABLE IF NOT EXISTS `actor_ids`(
+        `actor_id_i` int(11) unsigned NOT NULL,
+        `actor_id` varchar(100),
+        `name_unified` text
+    ) ENGINE=Aria PAGE_CHECKSUM=0 TRANSACTIONAL=0;
+    """
+    index = """
+    ALTER TABLE `actor_ids`
+        ADD PRIMARY KEY (`actor_id_i`),
+        ADD INDEX IF NOT EXISTS `actor_composite` (`actor_id`,`actor_id_i`);
+    """
+    metadata = load_table(spark,table,processed_bucket,database,schema,index)
+    return Output(None,metadata=metadata)
 
-        
+@asset(
+    deps=["edition_authors"],
+    group_name="database"
+)
+def db_actor_ids() -> Output[None]:
+    spark = get_spark_session(application_name="Load MariaDB")
+    table = "edition_authors"
+    database = os.getenv('DB_DATABASE')
+    schema = """
+    CREATE TABLE IF NOT EXISTS `edition_authors` (
+        `edition_id_i` int(11) unsigned NOT NULL,
+        `actor_id_i` int(11) unsigned DEFAULT NULL
+    )ENGINE=Aria PAGE_CHECKSUM=0 TRANSACTIONAL=0;
+    """
+    index = """
+    ALTER TABLE `edition_authors`
+        ADD INDEX IF NOT EXISTS `edition_id_i` (`edition_id_i`),
+        ADD INDEX IF NOT EXISTS `actor_id_i` (`actor_id_i`);
+    """
+    metadata = load_table(spark,table,processed_bucket,database,schema,index)
+    return Output(None,metadata=metadata)
+
+@asset(
+    deps=["textreuse_source_lengths"],
+    group_name="database"
+)
+def db_actor_ids() -> Output[None]:
+    spark = get_spark_session(application_name="Load MariaDB")
+    table = "textreuse_source_lengths"
+    database = os.getenv('DB_DATABASE')
+    schema = """
+    CREATE TABLE IF NOT EXISTS `textreuse_source_lengths` (
+        `trs_id` int(11) unsigned NOT NULL,
+        `text_length` int(11) unsigned DEFAULT NULL
+    )ENGINE=Aria PAGE_CHECKSUM=0 TRANSACTIONAL=0;
+    """
+    index = """
+    ALTER TABLE `textreuse_source_lengths`
+        ADD PRIMARY KEY (`trs_id`);
+    """
+    metadata = load_table(spark,table,processed_bucket,database,schema,index)
+    return Output(None,metadata=metadata)
