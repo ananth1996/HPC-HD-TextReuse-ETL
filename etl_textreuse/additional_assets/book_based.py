e501cad0c39f3dd68fe6168d2f3f31fd8882ad54
Ananth Mahadevan <ananth.mahadevan@helsinki.fi>
2024-10-16 17:53:55 +0300

    Adds Ville coverage assets for

--- etl_textreuse/additional_assets/book_based.py
+++ etl_textreuse/additional_assets/book_based.py
@@ -293,7 +293,7 @@ def reception_inception_between_book_coverages() -> None:
 def db_earliest_book_and_pieces_by_cluster() -> Output[None]:
     spark = get_spark_session(application_name="Load MariaDB")
     table = "earliest_book_and_pieces_by_cluster"
-    database = "hpc-hd-newspapers"
+    database = os.getenv("DB_DATABASE")
     schema = """
     CREATE TABLE IF NOT EXISTS `earliest_book_and_pieces_by_cluster`(
         `cluster_id` int(11) unsigned NOT NULL,
@@ -317,7 +317,7 @@ def db_earliest_book_and_pieces_by_cluster() -> Output[None]:
 def db_reception_edges_between_books_denorm() -> Output[None]:
     spark = get_spark_session(application_name="Load MariaDB")
     table = "reception_edges_between_books_denorm"
-    database = "hpc-hd-newspapers"
+    database = os.getenv("DB_DATABASE")
     schema = """
     CREATE TABLE IF NOT EXISTS `reception_edges_between_books_denorm`(
         `src_trs_id` int(11) unsigned NOT NULL,
@@ -332,6 +332,39 @@ def db_reception_edges_between_books_denorm() -> Output[None]:
     ALTER TABLE `reception_edges_between_books_denorm`
     ADD INDEX IF NOT EXISTS `src_trs_id` (`src_trs_id`),
     ADD INDEX IF NOT EXISTS `dst_trs_id` (`dst_trs_id`);
+    """
+    metadata = load_table(spark,table,processed_bucket,database,schema,index)
+    
+    return Output(None,metadata=metadata)
+
+@asset(
+    deps=["reception_inception_between_book_coverages"],
+    group_name="additional_assets_for_ville"
+)
+def db_reception_inception_between_book_coverages() -> Output[None]:
+    spark = get_spark_session(application_name="Load MariaDB")
+    table = "reception_inception_between_book_coverages"
+    database = os.getenv("DB_DATABASE")
+    schema = """
+    CREATE TABLE IF NOT EXISTS `reception_inception_between_book_coverages` (
+        `src_trs_id` int(11) unsigned NOT NULL,
+        `num_reuses_src` int(11) unsigned DEFAULT NULL,
+        `reuses_src_in_dst` int(11) unsigned DEFAULT NULL,
+        `src_length` int(11) unsigned DEFAULT NULL,
+        `coverage_src_in_dst` double unsigned DEFAULT NULL,
+        `dst_trs_id` int(11) unsigned NOT NULL,
+        `num_reuses_dst` int(11) unsigned DEFAULT NULL,
+        `reuses_dst_in_src` int(11) unsigned DEFAULT NULL,
+        `dst_length` int(11) unsigned DEFAULT NULL,
+        `coverage_dst_in_src` double unsigned DEFAULT NULL
+    )ENGINE=Aria PAGE_CHECKSUM=0 TRANSACTIONAL=0;
+    """
+    index = """
+    ALTER TABLE `reception_inception_between_book_coverages`
+        ADD INDEX IF NOT EXISTS `src_trs_id` (`src_trs_id`),
+        ADD INDEX IF NOT EXISTS `reception` (`coverage_src_in_dst`),
+        ADD INDEX IF NOT EXISTS `dst_trs_id` (`dst_trs_id`),
+        ADD INDEX IF NOT EXISTS `inception` (`coverage_dst_in_src`);
     """
     metadata = load_table(spark,table,processed_bucket,database,schema,index)
     
